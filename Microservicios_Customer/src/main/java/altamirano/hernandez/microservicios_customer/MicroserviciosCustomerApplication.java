e949985c1d23a708dcd141851a3fabec079e9506
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-06-24 10:34:49 -0600

    initial commit

---
+++ Microservicios_Customer/src/main/java/altamirano/hernandez/microservicios_customer/MicroserviciosCustomerApplication.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.microservicios_customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviciosCustomerApplication.class, args);
+    }
+
+}
---
+++ Microservicios_Customer/src/test/java/altamirano/hernandez/microservicios_customer/MicroserviciosCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.microservicios_customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
